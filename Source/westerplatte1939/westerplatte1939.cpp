5797ab55c8d4195252054be14c84b4a4f6ccf29d
GreeninZ <lukasz.staporek15@gmail.com>
2022-10-30 17:59:30 +0100

    Revert "Utworzenie projektu + dodanie .gitignore dla UE"
    This reverts commit 6cfdce08573f3279dbb4a2aaf76fdfdf7b880a6e.

--- Source/westerplatte1939/westerplatte1939.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "westerplatte1939.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, westerplatte1939, "westerplatte1939" );
